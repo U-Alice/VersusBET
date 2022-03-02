8c6caf001dbf2acde74471fa1d362eb22c872652
Hallcoder <hallcoder25@gmail.com>
2022-03-02 06:58:00 +0200

    added functionality

--- signUp.js
+++ signUp.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const { userSchema } = require('./userSchema');
 const app = express();
 const PORT  = process.env.PORT || 4000;
+const URL = "mongodb+srv://zesta:!!!%3F!%3F123Abana@versusbet.bwxby.mongodb.net/test?authSource=admin&replicaSet=atlas-ob0mnz-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+mongoose.connect(URL)
+.then(_ => console.log("Connected successfully!Q..."))
 
 app.listen(PORT,()=>{
     console.log(`Listening on PORT: |${PORT}|`);
@@ -10,4 +14,22 @@ app.listen(PORT,()=>{
 
 app.get('/',(req,res)=>{
     res.send("Hello world...")
+});
+
+app.post('/signUp',async(req,res)=>{
+    try {
+        const user = new userSchema({
+            firstName:req.body.firstName,
+            lastName:req.body.lastName,
+            email:req.body.email,
+            tel:req.body.tel,
+            country:req.body.country,
+            password:req.body.password
+        });
+         await user.save()
+    } catch (error) {
+        res.send(error.message)
+    }
+   
+
 })
---
+++ userSchema.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const emailMongo = require('mongoose-type-email');
+const userSchema = new mongoose.Schema({
+       email:{
+           type:mongoose.SchemaTypes.Email,
+           required:true
+       },
+       firstName:{
+           type:String,
+           minlength:1,
+           maxlength:100,
+           required:true
+       },
+       lastName:{
+        type:String,
+        minlength:1,
+        maxlength:100,
+        required:true
+       },
+       tel:{
+           type:String,
+           minlength:10,
+           maxlength:10,
+           required:true
+       },
+       password:{
+           type:String,
+           minlength:8,
+           maxlength:15,
+           required:true
+       },
+       country:{
+         type:String,
+         required:true
+       }
+});
+
+module.exports.userSchema = mongoose.model('users',userSchema);
